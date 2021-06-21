f7fb5b355eff3b60c260ea5d92ea5a1d46dfa881
Mateusz Majewski <m.majewski2@student.mini.pw.edu.pl>
2021-06-21 23:36:13 +0200

    Clean up the code a bit

--- include/des.h
+++ include/des.h
@@ -1,11 +1,10 @@
 #pragma once
 #include <stdbool.h>
 
-
 struct result {
     double best_fit;
     double *best_result;
     int count;
 };
 
-struct result des(int N, double initial_point[N], double function_fn(int N, double[N]), double lower[N], double upper[N], int seed, bool logRes);
+struct result des(int N, double function_fn(int N, double[N]), double lower[N], double upper[N], int seed, bool logRes);
